bbe6af6cf9846d31c025bdc592db180c2d38f19f
João Rodrigues <jotar910@gmail.com>
2022-09-10 16:55:03 +0100

    setup eslint for typescript

--- src/api/components/index.ts
+++ src/api/components/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { MovieRoutes } from "./movie/routes";
 
-export function registerApiRoutes(router: Router, prefix: string = ''): void {
+export function registerApiRoutes(router: Router, prefix = ''): void {
 	router.use(`${prefix}/movies`, new MovieRoutes().router);
 }
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 import { registerApiRoutes } from './components';
 
 export function initRestRoutes(router: Router): void {
-    const prefix: string = '/api/v1';
+    const prefix = '/api/v1';
 
     router.get(prefix, (_: Request, res: Response) => res.send('OK'));
 
